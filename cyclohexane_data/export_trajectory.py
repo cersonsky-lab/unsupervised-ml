f05310846356fc8c42f6e1939e01a80732edfbb8
Rose K. Cersonsky <47536110+rosecers@users.noreply.github.com>
2022-02-21 18:57:24 +0100

    Replacing metadynamics simulations with MD

--- cyclohexane_data/export_trajectory.py
+++ cyclohexane_data/export_trajectory.py
@@ -1,4 +1,5 @@
 from ase.io import read, write
+import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser(
@@ -11,17 +12,38 @@ parser.add_argument(
     type=str,
     nargs="+",
     help="filename to read",
-    default="md.out",
+    default=["md.out"],
 )
+
+parser.add_argument(
+    "-o",
+    "--output",
+    metavar="f",
+    type=str,
+    nargs="+",
+    help="filename to export",
+    default=["trajectory.xyz"],
+)
+
+parser.add_argument("-t", "--trim", type=int, nargs=1, default=None)
 
 args = parser.parse_args()
 
-fn = args.filename
+fn = args.filename[0]
+ofn = args.output[0]
 frames = read(fn, ":")
+
+if args.trim is not None:
+    frames = frames[: args.trim[0]]
 
 for frame in frames:
     frame.info["energy_eV"] = frame.get_total_energy()
-    frame.arrays["forces_eV_per_au"] = frame.get_forces()
+    try:
+        frame.arrays["forces_eV_per_au"] = frame.get_forces()
+    except RuntimeError:
+        pass
+    frame.positions -= np.mean(frame.positions, axis=0)
+    frame.positions += np.diag(frame.cell) / 2
 
-write("trajectory.xyz", frames)
-write("final.xyz", frames[-1])
+print(fn, len(frames))
+write(ofn, frames)
